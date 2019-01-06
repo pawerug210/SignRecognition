05800b0053c453014f27540800dddfcfabc65003
Pawel Rugor <pawerug210@student.polsl.pl>
2019-01-06 22:02:18 +0000

    working v1, 2 convolutional layers, without bias, plotting loss and accuracy

--- scripts/CNN.py
+++ scripts/CNN.py
@@ -16,15 +16,14 @@ class CNN(object):
         self.yPlaceholder = tf.placeholder("float", [None, outputsNumber])
 
         self.WEIGHTS = self.initializeWeights()
-        pass
 
     def setOutputsNumber(self, number):
         self.OUTPUTS = number
 
     def initializeWeights(self):
         weights = {
-            'wd1': tf.get_variable('W3', shape=(2*2*128, 128), initializer=tf.contrib.layers.xavier_initializer()),
-            'out': tf.get_variable('W6', shape=(128, self.OUTPUTS), initializer=tf.contrib.layers.xavier_initializer()),
+            'wd1': tf.get_variable('W3', shape=(4*4*64, 64), initializer=tf.contrib.layers.xavier_initializer()),
+            'out': tf.get_variable('W6', shape=(64, self.OUTPUTS), initializer=tf.contrib.layers.xavier_initializer()),
         }
         for i in range(0, self.CONV_LAYERS):
             weights['wc' + str(i + 1)] = tf.get_variable('W' + str(i), shape=self.FILTERS_SHAPE[i], initializer=tf.contrib.layers.xavier_initializer())
@@ -33,7 +32,7 @@ class CNN(object):
     def conv2d(self, inputs_batch, filters, b, strides=1):
         # Conv2D wrapper, with bias and relu activation
         inputs_batch = tf.nn.conv2d(inputs_batch, filters, strides=[1, strides, strides, 1], padding='SAME')
-        inputs_batch = tf.nn.bias_add(inputs_batch, b)
+        # inputs_batch = tf.nn.bias_add(inputs_batch, b)
         return tf.nn.relu(inputs_batch)
 
     def maxpool2d(self, x, k=2):
@@ -44,27 +43,27 @@ class CNN(object):
         biases = self.BIAS
         # here we call the conv2d function we had defined above and pass the input image x, weights wc1 and bias bc1.
         conv1 = self.conv2d(input, filters['wc1'], biases['bc1'])
-        # Max Pooling (down-sampling), this chooses the max value from a 2*2 matrix window and outputs a 14*14 matrix.
+        # Max Pooling (down-sampling), this chooses the max value from a 2*2 matrix window and outputs a 8*8 matrix.
         conv1 = self.maxpool2d(conv1, k=2)
 
         # Convolution Layer
         # here we call the conv2d function we had defined above and pass the input image x, weights wc2 and bias bc2.
         conv2 = self.conv2d(conv1, filters['wc2'], biases['bc2'])
-        # Max Pooling (down-sampling), this chooses the max value from a 2*2 matrix window and outputs a 7*7 matrix.
+        # Max Pooling (down-sampling), this chooses the max value from a 2*2 matrix window and outputs a 4*4 matrix.
         conv2 = self.maxpool2d(conv2, k=2)
 
-        conv3 = self.conv2d(conv2, filters['wc3'], biases['bc3'])
+        # conv3 = self.conv2d(conv2, filters['wc3'], biases['bc3'])
         # Max Pooling (down-sampling), this chooses the max value from a 2*2 matrix window and outputs a 4*4.
-        conv3 = self.maxpool2d(conv3, k=2)
+        # conv3 = self.maxpool2d(conv3, k=2)
 
         # Fully connected layer
         # Reshape conv2 output to fit fully connected layer input
-        fc1 = tf.reshape(conv3, [-1, filters['wd1'].get_shape().as_list()[0]])
-        fc1 = tf.add(tf.matmul(fc1, filters['wd1']), biases['bd1'])
+        fc1 = tf.reshape(conv2, [-1, filters['wd1'].get_shape().as_list()[0]])
+        fc1 = tf.matmul(fc1, filters['wd1'])
         fc1 = tf.nn.relu(fc1)
         # Output, class prediction
         # finally we multiply the fully connected layer with the weights and add a bias term.
-        out = tf.add(tf.matmul(fc1, filters['out']), biases['out'])
+        out = tf.matmul(fc1, filters['out'])
         return out
 
     def run(self, session, fetches, batchX, batchY):
--- scripts/Program.py
+++ scripts/Program.py
@@ -2,11 +2,12 @@ import Common
 from DataPreprocessing import resize
 from DataPreprocessing import labelsToOutputs
 from DataPreprocessing import createMapping
+import matplotlib.pyplot as plt
 import tensorflow as tf
 import CNN
 
 #training settings
-iterations = 1000
+epochs = 100
 batchSize = 32
 learningRate = 0.001
 
@@ -14,8 +15,8 @@ learningRate = 0.001
 testSamplesNumber = 20
 
 #CNN settings
-classes = range(0, 2)
-samplesNumber = 20
+classes = range(10, 20)
+samplesNumber = 200
 inputImgSize = (16, 16)
 # filter (3x3), 3 channels (RGB), number of filters applied
 filtersShape = [(3, 3, 3, 32),
@@ -52,8 +53,14 @@ testImages, testLabels = Common.readTestImages('../Data/GTSRB_Final_Test_Images/
 testImages = resize(testImages, inputImgSize)
 testOutputs = labelsToOutputs(classes, testLabels)
 
+# Common.displayLabelsHist(testLabels)
+# Common.displayImagesSample(testImages)
+
 if len(testImages) != len(testOutputs):
     raise Exception
+if not all([any(x) for x in testOutputs]):
+    raise ValueError('Not all outputs have value')
+
 
 classesMapping = createMapping(classes)
 
@@ -76,21 +83,51 @@ init = tf.global_variables_initializer()
 
 with tf.Session() as session:
     session.run(init)
-    train_loss = []
+    train_loss_global = []
+    train_accuracy_global = []
     test_loss = []
-    train_accuracy = []
     test_accuracy = []
     summary_writer = tf.summary.FileWriter('./Output', session.graph)
-    for i in range(iterations):
+    for i in range(epochs):
+        train_loss = []
+        train_accuracy = []
         for k in range(len(batchesX)):
             trainBatchX = batchesX[k]
             trainBatchY = batchesY[k]
             op = cNN.run(session, optimizer, trainBatchX, trainBatchY)
             loss, acc = cNN.run(session, [cost, accuracy], trainBatchX, trainBatchY)
+            train_accuracy.append(acc)
+            train_loss.append(loss)
+        avg_train_loss = sum(train_loss) / len(train_loss)
+        avg_train_acc = sum(train_accuracy) / len(train_accuracy)
         print("Iter " + str(i) + ", Loss= " + \
-              "{:.6f}".format(loss) + ", Training Accuracy= " + \
-              "{:.5f}".format(acc))
-        test_acc, valid_loss = cNN.run(session, [accuracy, cost], testImages, testOutputs)
+              "{:.6f}".format(avg_train_loss) + ", Training Accuracy= " + \
+              "{:.5f}".format(avg_train_acc))
+        valid_loss, test_acc = cNN.run(session, [cost, accuracy], testImages, testOutputs)
         print("Testing Accuracy:", "{:.5f}".format(test_acc))
+        print("Testing Loss:", "{:.5f}".format(valid_loss))
+        train_accuracy_global.append(avg_train_acc * 100.0)
+        train_loss_global.append(avg_train_loss)
+        test_accuracy.append(test_acc * 100.0)
+        test_loss.append(valid_loss)
+
+plt.subplot(1, 2, 1)
+plt.plot(range(0, epochs), train_loss_global, 'b', label='train')
+plt.plot(range(0, epochs), test_loss, 'r', label='test')
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.title('Loss')
+plt.legend(loc='upper left')
+
+plt.subplot(1, 2, 2)
+plt.plot(range(0, epochs), train_accuracy_global, 'b', label='train')
+plt.plot(range(0, epochs), test_accuracy, 'r', label='test')
+plt.xlabel('epoch')
+plt.ylabel('accuracy [%]')
+plt.title('Accuracy')
+plt.legend(loc='upper left')
+
+plt.show()
+
 
 
